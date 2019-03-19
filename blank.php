b62ddfb053f8e6303d6a1a5195fb9050c6e12438
Vivekgupta2227 <40454812+Vivekgupta2227@users.noreply.github.com>
2019-03-19 21:22:28 +0530

    Add files via upload

--- blank.php
+++ blank.php
@@ -131,10 +131,7 @@
 
       <!-- Sidebar - Brand -->
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
-          <i class="fas fa-laugh-wink"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</div>
+        <div class="sidebar-brand-text mx-3"><img src="img/logo-01.jpg" style="height: 50px; width: 160px; border-radius: 5px 5px 5px 5px;"></div>
       </a>
 
       <!-- Divider -->
